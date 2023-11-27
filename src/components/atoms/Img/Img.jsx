639ea77b4e3c26a8372609056974008b60653fa3
Edgar <jorge260590@hotmail.com>
2023-11-27 18:23:27 -0500

    feat(gallery section): Add the gallery section, with the logic to make responsive design and image modal

--- src/components/atoms/Img/Img.jsx
+++ src/components/atoms/Img/Img.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types'
 
-const Img = ({altImg, className}) => {
+const Img = ({altImg, className, onClick}) => {
     const routeFile = `/public/assets/img/${altImg}.png`;
 
   return (
     <>
-        <img className={className} src={routeFile} alt={altImg} />
+        <img className={className} src={routeFile} alt={altImg} onClick={onClick}/>
     </>
   )
 }
 
 Img.propTypes = {
-    altImg: PropTypes.string,
-    className: PropTypes.string
+  onClick: PropTypes.func,
+  altImg: PropTypes.string,
+  className: PropTypes.string
 }
 
 export default Img
--- src/components/organisms/GalleryContainer/GalleryContainer.jsx
+++ src/components/organisms/GalleryContainer/GalleryContainer.jsx
@@ -1,9 +1,45 @@
-
+import Img from "../../atoms/Img/Img";
+import {useEffect, useState} from 'react';
+import './GalleryContainer.scss';
 const GalleryContainer = () => {
+
+  const [data, setData] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImg, setSelectedImg] = useState('');
+
+  useEffect(() => {
+    fetch('src/db/GalleryAltImg.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log('Error Data Base', error))
+  },[])
+  
+  const handleModal = (srcImg) => {
+    setModalOpen(true)
+    setSelectedImg(srcImg)
+  }
+
+  const closeModal = () =>{
+    setModalOpen(false)
+  }
+  console.log(modalOpen)
   return (
     <div className="gallery">
         <div className="gallery__container">
+          {data.map((item, index)=>{
+            return(
+              <Img className={'gallery__img'} 
+              key={index} 
+              altImg={item.altImg}
+              onClick={() => handleModal(item.altImg)}/>
+
+            )
+          })
+          }
         </div>
+          <div className={`gallery__modal ${modalOpen ? 'gallery__modal--active':''}`} onClick={closeModal}>
+            <Img className={'gallery__modalImg'} altImg={selectedImg}/>
+          </div>
     </div>
   )
 }
